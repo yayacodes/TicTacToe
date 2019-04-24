90238cc4f35212559bbd21207071e198a2c9bdeb
yehiakawad <yehiakawad@gmail.com>
2019-04-24 18:30:18 -0400

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,3 @@
-// On my honor:
-//
-// - I have not used code obtained from another student,
-// or any other unauthorized source, either modified or unmodified.
-//
-// - All code and documentation used in my program is either my original
-// work, or was derived, by me, from the source code provided by the assignment.
-//
-// - I have not discussed coding details about this project with anyone
-// other than my instructor, teaching assistants assigned to this
-// course, except via the message board for the course. I understand that I
-// may discuss the concepts of this program with other students, and that
-// another student may help me debug my program so long as neither of us
-// writes anything during the discussion or modifies any computer file
-// during the discussion. I have violated neither the spirit nor letter of this restriction.
-//
 import java.util.*;
 
 public class TicTacToe {
